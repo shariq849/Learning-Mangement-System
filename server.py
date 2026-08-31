adf8b5740b12250241c63fd236ffec7f407294bd
Shariq Ahmad <shariq849@gmail.com>
2026-08-31 21:00:58 +0530

    changes made

--- server.py
+++ server.py
@@ -1,5 +1,11 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import Mapped,mapped_column,DeclarativeBase
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///registerUsers.db'
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db = SQLAlchemy(app)
+
 @app.route("/")
 def login():
     return render_template("index.html")
